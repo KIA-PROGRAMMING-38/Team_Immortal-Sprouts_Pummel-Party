862b1802ec7351f0f480844812a0d5717fdc790f
EunSu <eunning09005@gmail.com>
2023-06-09 16:05:28 +0900

    [Feature] LeaveGame Button 기능 구현 및 Title 기능 수정
    1) MultiGameCanvas에 LeaveGame Button을 추가하여 게임을 종료할 수 있는 기능을 추가하였습니다.
    2) Title에서 InputSystem으로 사용자의 입력을 받아오는 것에서 Button 기능으로 입력을 받아오게 수정하였습니다.

--- Team_Immortal Sprouts_Pummel Party/Assets/Scripts/Lobby/Title.cs
+++ Team_Immortal Sprouts_Pummel Party/Assets/Scripts/Lobby/Title.cs
@@ -9,30 +9,37 @@ using UnityEngine.InputSystem.EnhancedTouch;
 public class Title : MonoBehaviour
 {
     [SerializeField] private GameObject MultiPlayerCanvas;
-    private PlayerInput playerInput;
-    private InputAction touchAction;
+    //private PlayerInput playerInput;
+    //private InputAction touchAction;
 
-    private void Awake()
-    {
-        playerInput = GetComponent<PlayerInput>();
-        touchAction = playerInput.actions["Touch"];
-    }
+    //private void Awake()
+    //{
+    //    playerInput = GetComponent<PlayerInput>();
+    //    touchAction = playerInput.actions["Touch"];
+    //}
 
-    private void OnEnable()
-    {
-        touchAction.performed -= TouchTitle;
-        touchAction.performed += TouchTitle;
-    }
+    //private void OnEnable()
+    //{
+    //    touchAction.performed -= TouchTitle;
+    //    touchAction.performed += TouchTitle;
+    //}
 
-    private void OnDisable()
-    {
-        touchAction.performed -= TouchTitle;
-    }
+    //private void OnDisable()
+    //{
+    //    touchAction.performed -= TouchTitle;
+    //}
 
-    private void TouchTitle(InputAction.CallbackContext value)
+    //private void TouchTitle(InputAction.CallbackContext value)
+    //{
+    //    gameObject.transform.parent.gameObject.SetActive(false);
+    //    MultiPlayerCanvas.SetActive(true);
+    //    Debug.Log("Touch Title");
+    //}
+
+    public void OnTouchTitle()
     {
         gameObject.transform.parent.gameObject.SetActive(false);
         MultiPlayerCanvas.SetActive(true);
         Debug.Log("Touch Title");
-    }
+    }    
 }
