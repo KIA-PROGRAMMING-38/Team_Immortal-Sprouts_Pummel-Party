cf486833f64e8853e670ae7a24f6dd997ad980c2
rickythediver <wooseok1995@gmail.com>
2023-06-09 17:06:22 +0900

    [Modify] 섬 프리팹 tag 및 layer 설정 + 특정 오브젝트 노말맵 제거
    1) 섬 프리팹 tag 및 layer 설정
    2) 특정 오브젝트 노말맵 제거 (연산 아끼기 위해)
    - 불가사리
    - 돌
    - 코인
    - 회전섬 화살표

--- Team_Immortal Sprouts_Pummel Party/Assets/Scripts/Islands/Island.cs
+++ Team_Immortal Sprouts_Pummel Party/Assets/Scripts/Islands/Island.cs
@@ -10,41 +10,89 @@ public abstract class Island : MonoBehaviour
     [SerializeField] Island prevPlayerSpot;
     [SerializeField] Transform currentPlayerSpot;
 
-
+    [SerializeField] private bool isPlayerPresent; // 테스트 위해 SerializeField 입력
     [SerializeField] private Vector3 nextPosition; // 테스트 위해 SerializeField 입력
     [SerializeField] private Vector3 prevPosition; // 테스트 위해 SerializeField 입력
     [SerializeField] private Vector3 currentPosition; // 테스트 위해 SerializeField 입력
 
+    /// <summary>
+    /// 현재 섬의 플레이어 존재여부를 반환하는 함수
+    /// </summary>
+    /// <returns></returns>
+    public bool GetPlayerPresence()
+    {
+        return isPlayerPresent;
+    }
 
+    /// <summary>
+    /// 현재 섬의 플레이어 존재여부를 설정할 수 있는 함수
+    /// </summary>
+    /// <param name="_isPlayerOut"></param>
+    public void SetPlayerPresence(bool _isPlayerOut)
+    {
+        isPlayerPresent = _isPlayerOut;
+    }
 
+    /// <summary>
+    /// 현재 섬의 플레이어 위치를 반환하는 함수
+    /// </summary>
+    /// <returns></returns>
     public Vector3 GetCurrentPosition()
     {
-        Debug.Assert(currentPosition != null);
         return currentPosition;
     }
 
+    /// <summary>
+    /// 플레이어가 향할 다음 섬의 위치를 반환하는 함수
+    /// </summary>
+    /// <returns></returns>
     public Vector3 GetNextPosition()
     {
         Debug.Assert(nextPosition != null);
         return nextPosition;
     }
 
+    /// <summary>
+    /// 플레이어가 향할 이전 섬의 위치를 반환하는 함수
+    /// </summary>
+    /// <returns></returns>
     public Vector3 GetPrevPosition()
     {
         Debug.Assert(prevPosition != null);
         return prevPosition;
     }
 
-    public void SetNextPosition()
+    /// <summary>
+    /// 현재 섬의 플레이어 위치를 설정하는 함수
+    /// </summary>
+    /// <param name="_position"></param>
+    public void SetCurrentPosition(Vector3 _position)
     {
-
+        currentPosition = _position;
     }
 
-    public void SetPrevPosition()
+    /// <summary>
+    /// 플레이어가 향할 다음 섬의 위치를 설정하는 함수
+    /// </summary>
+    /// <param name="_nextPosition"></param>
+    public void SetNextPosition(Vector3 _nextPosition)
     {
-
+        nextPosition = _nextPosition;
     }
 
+    /// <summary>
+    /// 플레이어가 향할 이전 섬의 위치를 설정하는 함수
+    /// </summary>
+    /// <param name="_prevPosition"></param>
+    public void SetPrevPosition(Vector3 _prevPosition)
+    {
+        prevPosition = _prevPosition;
+    }
+
+    /// <summary>
+    /// 현재섬의 다음, 이전섬의 위치를 초기화하는 함수
+    /// </summary>
+    /// <returns></returns>
     protected async UniTaskVoid InitPositionSettings()
     {
         if (currentPlayerSpot != null)
--- Team_Immortal Sprouts_Pummel Party/Assets/Scripts/Islands/NobodyIsland.cs
+++ Team_Immortal Sprouts_Pummel Party/Assets/Scripts/Islands/NobodyIsland.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class NobodyIsland : Island
 {
-    [SerializeField] Transform[] nobodyIslandPlayerSpots;
-    
+    [SerializeField] private Transform[] currentPlayerSpots;
     void Start()
     {
         InitPositionSettings().Forget();
+        SetCurrentPosition(currentPlayerSpots[0].position);
     }
 
     // Update is called once per frame
