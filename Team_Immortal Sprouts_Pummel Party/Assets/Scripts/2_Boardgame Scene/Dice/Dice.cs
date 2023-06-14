c0de38e15b81538caf5af0ed68e22e5fc854c62d
EunSu <eunning09005@gmail.com>
2023-06-14 18:07:10 +0900

    [Feature] Dice 회전 및 플레이어의 입력을 감지하여 주사위 값 출력 구현
    1) 플레이어가 화면을 터치하면 주사위의 회전이 멈추는 것을 구현했습니다.

--- Team_Immortal Sprouts_Pummel Party/Assets/Scripts/2_Boardgame Scene/Dice/Dice.cs
+++ Team_Immortal Sprouts_Pummel Party/Assets/Scripts/2_Boardgame Scene/Dice/Dice.cs
@@ -8,63 +8,31 @@ using UnityEngine.InputSystem;
 public class Dice : MonoBehaviour
 {
     [SerializeField] private GameObject dice;
-    private float rotateZSpeed = 800f;
+    private BoardgamePlayer player;
+    private float rotateZSpeed = 800f; 
     private float rotateYSpeed = 800f;
     private bool isClickedBoardGameScreen = false;
-    private PlayerInput playerInput;
-    private InputAction touchPressAction;
+    private bool isRollDice = false;
 
     private void Awake()
     {
-        //playerInput = GetComponent<PlayerInput>();
-        //touchPressAction = playerInput.actions["ScreenTouch"];
+        player = GetComponent<BoardgamePlayer>();
     }
 
+    /// <summary>
+    /// 플레이어의 터치 입력을 받아오는 함수입니다.
+    /// </summary>
     public void OnTouchRollDicePanel()
     {
         isClickedBoardGameScreen = true;
         Debug.Log("터치됨");
     }
 
-    //private void OnEnable()
-    //{
-    //    touchPressAction.performed -= OnTouchBoardGameScreen;
-    //    touchPressAction.performed += OnTouchBoardGameScreen;
-    //}
-
-    //private void OnDisable()
-    //{
-    //    touchPressAction.performed -= OnTouchBoardGameScreen;
-    //}
-
-    float elapsedTime = 0;
-    float rotationTime = 10f;
-
     /// <summary>
-    /// 임시 기능.. 화면 터치 입력 발생했을 때 주사위 굴릴 수 있도록..
+    /// 주사위의 값을 출력하는 함수입니다.
     /// </summary>
-    public int Roll()
+    public void Roll()
     {
-        //int diceResult = 0;
-        //elapsedTime += Time.deltaTime;
-
-        //// 주사위 회전
-        //// 임시 조건 지정
-        //while (elapsedTime <= rotationTime)
-        //{
-        //    OnAppearDice();
-        //    dice.transform.Rotate(0, 0, rotateSpeed * Time.deltaTime);
-
-        //    if (isClickedBoardGameScreen == true)
-        //    {
-        //        Debug.Log("if문 들어옴");
-        //        diceResult = Random.Range(-1, 8);
-        //        dice.transform.rotation = GetRotationValue(diceResult);
-        //        Debug.Log($"주사위 결과: {diceResult}");
-
-        //        break;
-        //    }
-        //}
         int diceResult = 0;
 
         if (isClickedBoardGameScreen == false)
@@ -73,17 +41,16 @@ public class Dice : MonoBehaviour
             dice.transform.Rotate(0, rotateYSpeed * Time.deltaTime, rotateZSpeed * Time.deltaTime);
         }
 
-        else
+        else if (isClickedBoardGameScreen == true && isRollDice == false)
         {
-            diceResult = Random.Range(-1, 8);
+            diceResult = Random.Range(-1, 0);
             dice.transform.rotation = GetRotationValue(diceResult);
+            isRollDice = true;
             Debug.Log($"주사위 결과: {diceResult}");
         }
 
-        //OnDisappearDice();
-
-        // elapsedTime = 0;
-        return diceResult;
+        player.SetMoveCount(diceResult);
+        //return diceResult;
     }
 
     private void Update()
@@ -91,16 +58,16 @@ public class Dice : MonoBehaviour
         Roll();
     }
 
-    Quaternion minusoneRotateValue = Quaternion.Euler(126, 425, 163);
-    Quaternion zeroRotateValue = Quaternion.Euler(126, 72, 169);
-    Quaternion oneRotateValue = Quaternion.Euler(19, -154, 136);
-    Quaternion twoRotateValue = Quaternion.Euler(-32, 424, 190);
-    Quaternion threeRotateValue = Quaternion.Euler(0, 353, 214);
-    Quaternion fourRotateValue = Quaternion.Euler(327, 93, 93);
-    Quaternion fiveRotateValue = Quaternion.Euler(31, 290, 194);
-    Quaternion sixRotateValue = Quaternion.Euler(-48, 125, -29);
-    Quaternion sevenRotateValue = Quaternion.Euler(0, 177, -48);
-    Quaternion errorRotateValue = Quaternion.Euler(31, -745, 46); // 아무 숫자도 없는 면
+    Quaternion minusoneRotateValue = Quaternion.Euler(128, -24, 166);
+    Quaternion zeroRotateValue = Quaternion.Euler(-21, 420, 142);
+    Quaternion oneRotateValue = Quaternion.Euler(18, 116, 145);
+    Quaternion twoRotateValue = Quaternion.Euler(-37, 338, 195);
+    Quaternion threeRotateValue = Quaternion.Euler(1, 265, 219);
+    Quaternion fourRotateValue = Quaternion.Euler(-25, 293, 40);
+    Quaternion fiveRotateValue = Quaternion.Euler(27, 199, -168);
+    Quaternion sixRotateValue = Quaternion.Euler(-37, 25, -18);
+    Quaternion sevenRotateValue = Quaternion.Euler(0, 94, -38);
+    Quaternion errorRotateValue = Quaternion.Euler(24, -124, 4); // 아무 숫자도 없는 면
 
     private Quaternion GetRotationValue(int diceResult)
     {
@@ -139,18 +106,18 @@ public class Dice : MonoBehaviour
     }
 
     /// <summary>
-    /// 주사위를 비활성화해주는 함수입니다.
-    /// </summary>
-    public void OnDisappearDice()
-    {
-        dice.gameObject.SetActive(false);
-    }
-
-    /// <summary>
     /// 주사위를 활성화해주는 함수입니다. 
     /// </summary>
     public void OnAppearDice()
     {
         dice.gameObject.SetActive(true);
+    }
+
+    /// <summary>
+    /// 주사위를 비활성화해주는 함수입니다.
+    /// </summary>
+    public void OnDisappearDice()
+    {
+        dice.gameObject.SetActive(false);
     }
 }
