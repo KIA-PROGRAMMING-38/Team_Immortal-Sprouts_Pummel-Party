ccdb00a54a5d449f1847bfc720df33cf1bb3d65e
0105kcy <ggbbsing@gmail.com>
2023-06-17 22:31:48 +0900

    [Style] 폴더 구조, 이름 변경
    1. Art 폴더에 SkyBox 데이터 추
    2. 빈 폴더 삭제
    3. VFX 폴더 제거 하고 파티클로 통
    4. Environments 폴더 추가
    5. Static Material 제거 하고 Materials로 통합
    6. Materials 하위 폴더 추가
    7. scriptable data scripts 폴더 생성
    8 Static GameObject 폴더 제거하고 통
    9 UI 폴더 생성

---
+++ Team_Immortal Sprouts_Pummel Party/Assets/Scripts/Scriptable/ModelData.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class ModelData : ScriptableObject
+{
+    public GameObject[] _LobbyModels;
+
+    public GameObject[] _BoardModels;
+
+    public GameObject[] _RoterRaceModel;
+
+    public GameObject[] _AwardModels;
+
+    public GameObject[] _InFightModels;
+}
---
+++ Team_Immortal Sprouts_Pummel Party/Assets/Scripts/Scriptable/PositionData.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class PositionData : ScriptableObject
+{
+    public Transform[] _LobbyPositions;
+
+    public Transform[] _BoardPositions;
+
+    public Transform[] _InFightPositions;
+
+    public Transform[] _RoterRacePositions;
+
+
+}
