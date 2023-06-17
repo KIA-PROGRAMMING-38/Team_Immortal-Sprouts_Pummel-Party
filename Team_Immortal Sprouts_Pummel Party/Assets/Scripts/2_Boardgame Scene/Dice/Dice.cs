47b469b48247e3f7a540537d0248403a9b660515
EunSu <eunning09005@gmail.com>
2023-06-17 19:22:12 +0900

    [Modify] Dice 스크립트 최적화

--- Team_Immortal Sprouts_Pummel Party/Assets/Scripts/2_Boardgame Scene/Dice/Dice.cs
+++ Team_Immortal Sprouts_Pummel Party/Assets/Scripts/2_Boardgame Scene/Dice/Dice.cs
@@ -6,133 +6,79 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Events;
 using Random = UnityEngine.Random;
-
 public class Dice : MonoBehaviour
 {
-    [SerializeField] private GameObject diceTotal;
-    [SerializeField] private BoardgamePlayer boardgamePlayer;
+    [SerializeField] private GameObject diceTotal; // 주사위오브젝트
     public UnityEvent OnDiceStopped;
-    private float rotateZSpeed = 800f; 
+    private float rotateZSpeed = 800f;
     private float rotateYSpeed = 800f;
     private bool isClickedBoardGameScreen = false;
-    private bool isRollDice = false;
 
     private void OnEnable()
     {
-        RollDice().Forget();
+        OnAppearDice();
     }
 
     /// <summary>
     /// 플레이어의 터치 입력을 받아오는 함수입니다.
     /// </summary>
-    public void OnTouchRollDicePanel()
+    public void OnTouchRollDicePanel() => isClickedBoardGameScreen = true;
+    private const int minResult = -1;
+    private const int maxResult = 8;
+    private int diceResult; // 주사위값
+    private async UniTaskVoid rotateDice() // 주사위가 회전하는 함수
     {
-        isClickedBoardGameScreen = true;
-    }
-
-    /// <summary>
-    /// 주사위의 값을 출력하는 함수입니다.
-    /// </summary>
-    public async UniTask RollDice()
-    {
-        int diceResult = 0;
-        WaitFor1Seconds();
-        OnAppearDice();
-
-        while (true)
+        while (!isClickedBoardGameScreen) // 터치가 없으면
         {
-            diceTotal.transform.Rotate(0, rotateYSpeed * Time.deltaTime, rotateZSpeed * Time.deltaTime);
-            
+            diceTotal.transform.Rotate(0, rotateYSpeed * Time.deltaTime, rotateZSpeed * Time.deltaTime); // 계속 돌아
             await UniTask.Yield();
-
-            if (isClickedBoardGameScreen == true && isRollDice == false)
-            {
-                diceResult = Random.Range(-1, 8);
-                diceTotal.transform.rotation = GetRotationValue(diceResult);
-                isRollDice = true;
-                Debug.Log($"주사위 결과: {diceResult}");
-                await WaitFor1SecondsDisappearDice();
-                boardgamePlayer.SetMoveCount(diceResult);
-                OnDiceStopped?.Invoke();
-
-                await UniTask.Yield();
-            }
         }
+
+        diceResult = Random.Range(minResult, maxResult);
+        diceTotal.transform.rotation = returnRotationValue(diceResult);
+        OnDiceStopped?.Invoke();
+        waitFor1SecondsDisappearDice().Forget();
     }
 
-    Quaternion minusoneRotateValue = Quaternion.Euler(128, 156, 166);
-    Quaternion zeroRotateValue = Quaternion.Euler(-21, 600, 142);
-    Quaternion oneRotateValue = Quaternion.Euler(18, 296, 145);
-    Quaternion twoRotateValue = Quaternion.Euler(-37, 518, 195);
-    Quaternion threeRotateValue = Quaternion.Euler(1, 445, 219);
-    Quaternion fourRotateValue = Quaternion.Euler(-25, 473, 40);
-    Quaternion fiveRotateValue = Quaternion.Euler(27, 379, -168);
-    Quaternion sixRotateValue = Quaternion.Euler(-37, 205, -18);
-    Quaternion sevenRotateValue = Quaternion.Euler(0, 274, -38);
-    Quaternion errorRotateValue = Quaternion.Euler(149, -109, 231); // 아무 숫자도 없는 면
+    public int ConveyDiceReuslt() => diceResult;
 
-    private Quaternion GetRotationValue(int diceResult)
+    private Quaternion returnRotationValue(int diceValue) // 주사위값에 해당하는 회전값을 반환하는 함수
     {
-        switch (diceResult)
-        {
-            case -1:
-                return minusoneRotateValue;
-                break;
-            case 0:
-                return zeroRotateValue;
-                break;
-            case 1:
-                return oneRotateValue;
-                break;
-            case 2:
-                return twoRotateValue;
-                break;
-            case 3:
-                return threeRotateValue;
-                break;
-            case 4:
-                return fourRotateValue;
-                break;
-            case 5:
-                return fiveRotateValue;
-                break;
-            case 6:
-                return sixRotateValue;
-                break;
-            case 7:
-                return sevenRotateValue;
-                break;
-            default:
-                return errorRotateValue;
-        }
+        int realIndex = diceValue + 1;
+        return rotationValues[realIndex];
     }
 
-    private async UniTask WaitFor1SecondsDisappearDice()
+    private async UniTaskVoid waitFor1SecondsDisappearDice()
     {
-        await UniTask.Delay(TimeSpan.FromSeconds(1f));
-        OnDisappearDice();
+        await WaitForSeconds(1f);
+        diceResult = 0;
+        onDisappearDice();
     }
 
-    private async UniTask WaitFor1Seconds()
-    {
-        await UniTask.Delay(TimeSpan.FromSeconds(1f));
-    }
+    private async UniTask WaitForSeconds(float waitTime) => await UniTask.Delay(TimeSpan.FromSeconds(waitTime));
 
     /// <summary>
-    /// 주사위를 활성화해주는 함수입니다. 
+    /// 주사위를 활성화해주는 함수입니다.
     /// </summary>
     public void OnAppearDice()
     {
         diceTotal.gameObject.SetActive(true);
         isClickedBoardGameScreen = false;
-        isRollDice = false; 
+        rotateDice().Forget();
     }
 
-    /// <summary>
-    /// 주사위를 비활성화해주는 함수입니다.
-    /// </summary>
-    public void OnDisappearDice()
+    private void onDisappearDice() => diceTotal.gameObject.SetActive(false);
+
+    private Quaternion[] rotationValues = new Quaternion[]
     {
-        diceTotal.gameObject.SetActive(false);
-    }
+        Quaternion.Euler(128, 156, 166), // 주사위값이 -1 이 나왔을때의 회전값
+        Quaternion.Euler(-21, 600, 142), // 주사위값이 0 이 나왔을때의 회전값
+        Quaternion.Euler(18, 296, 145), // 주사위값이 1 이 나왔을때의 회전값
+        Quaternion.Euler(-37, 518, 195), // 주사위값이 2 이 나왔을때의 회전값
+        Quaternion.Euler(1, 445, 219), // 주사위값이 3 이 나왔을때의 회전값
+        Quaternion.Euler(-25, 473, 40), // 주사위값이 4 이 나왔을때의 회전값
+        Quaternion.Euler(27, 379, -168), // 주사위값이 5 이 나왔을때의 회전값
+        Quaternion.Euler(-37, 205, -18), // 주사위값이 6 이 나왔을때의 회전값
+        Quaternion.Euler(0, 274, -38), // 주사위값이 7 이 나왔을때의 회전값
+    };
 }
