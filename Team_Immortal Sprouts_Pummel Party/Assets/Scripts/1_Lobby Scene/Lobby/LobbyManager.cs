b9f5f5496b58fd735e9784aa3d273f54b824f0ee
rickythediver <wooseok1995@gmail.com>
2023-06-20 02:05:15 +0900

    [Modify] 보드게임씬 설정 적용
    1) 폴더 정리
    2) 보드게임 회전섬 불 교체
    3) 움직이지 않는 오브젝트 static 처리(e.g. 섬)
    4) 쓸모 없는 콜라이더 제거
    e.g.  Flower3, BananaTree + Head, grass Set 2 and 3, coconut tree,  tree 1, 2, 3, 4 , SharkIsland Ribs Spine

---
+++ Team_Immortal Sprouts_Pummel Party/Assets/Scripts/1_Lobby Scene/Lobby/LobbyManager.cs
@@ -0,0 +1,55 @@
+using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LobbyManager : MonoBehaviourPunCallbacks
+{
+    private const string LOBBY_NAME = "Duck Duck Party";
+    private const string GAME_VERSION = "0.0.1";
+    private int repeatTime = 1;
+
+    [SerializeField] private GameObject touchGuide;
+    private void Start()
+    {
+        PhotonNetwork.AutomaticallySyncScene = true;
+        PhotonNetwork.GameVersion = GAME_VERSION;
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        touchGuide.SetActive(true);
+        if (!PhotonNetwork.InLobby)
+        {
+            PhotonNetwork.JoinLobby();
+        }
+
+        Debug.Log("서버에 연결되었습니다.");
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        PhotonNetwork.ConnectUsingSettings();
+
+        Debug.Log($"{cause}의 이유로 서버 연결에 실패하였습니다.");
+    }
+
+    public override void OnJoinedLobby()
+    {
+        if (0 < repeatTime)
+        {
+            repeatTime -= 1;
+            PhotonNetwork.LeaveLobby();
+        }
+        PhotonNetwork.CurrentLobby.Name = LOBBY_NAME;
+        Debug.Log("로비에 연결되었습니다.");
+    }
+
+    public override void OnLeftLobby()
+    {
+        PhotonNetwork.JoinLobby();
+        Debug.Log("로비를 떠났습니다.");
+    }
+}
