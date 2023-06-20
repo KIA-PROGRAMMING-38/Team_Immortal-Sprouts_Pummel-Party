8b3c8be9e3e59219020dd1b8b5b3209ddf05d560
minyeong <minyk0512@gmail.com>
2023-06-20 20:15:28 +0900

    [Feature] 무작위 아이템데이터 반환

--- Team_Immortal Sprouts_Pummel Party/Assets/Scripts/2_Boardgame Scene/Item/ItemProvider.cs
+++ Team_Immortal Sprouts_Pummel Party/Assets/Scripts/2_Boardgame Scene/Item/ItemProvider.cs
@@ -1,35 +1,64 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using static UnityEditor.Progress;
 
 public static class ItemProvider
 {
     public static ItemData[] ItemTable = Resources.LoadAll<ItemData>("ItemData");
     
     private static int totalWeight;
-    private static int TotalWeight
+
+    private static int getRandomNumber()
     {
-        get
+        if(totalWeight == 0)
         {
-            if(totalWeight != 0)
+            foreach (ItemData item in ItemTable)
             {
-                return totalWeight;
-            }
-            else
-            {
-                int weight = 0;
-
-                foreach (ItemData item in ItemTable)
-                {
-                    weight += item.Weight;
-                }
-
-                totalWeight = weight;
-                return totalWeight;
+                totalWeight += item.Weight;
             }
         }
 
-        set { totalWeight = value; }
+        int randomNumber = (int)(1f + Random.value * (totalWeight - 1) + 0.5f);
+
+        Debug.Log($"선정된 랜덤 숫자: {randomNumber}");
+        return randomNumber;
+    }
+
+    private static ItemData getRandomItem()
+    {
+        ItemData item = null;
+        int weight = 0;
+
+        int randomNumber = getRandomNumber();
+
+        for(int i = 0; i < ItemTable.Length; ++i)
+        {
+            weight += ItemTable[i].Weight;
+            if(randomNumber <= weight)
+            {
+                item = ItemTable[i];
+                break;
+            }
+        }
+
+        return item;
+    }
+
+    public static void Test()
+    {
+        int[] counts = new int[8];
+
+        for(int i = 0; i < 1000; ++i)
+        {
+            ItemData item = getRandomItem();
+            counts[item.Id] += 1;
+        }
+
+        int index = 0;
+
+        foreach(int count in counts)
+        {
+            Debug.Log($"{index++}: {count}");
+        }
     }
 }
