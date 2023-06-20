f655775bbcf8a618c7525958ffdf9d0256baa7db
minyeong <minyk0512@gmail.com>
2023-06-20 20:38:56 +0900

    [Feature] 플레이어에게 랜덤 아이템 지급 기능 구현

--- Team_Immortal Sprouts_Pummel Party/Assets/Scripts/2_Boardgame Scene/Item/ItemProvider.cs
+++ Team_Immortal Sprouts_Pummel Party/Assets/Scripts/2_Boardgame Scene/Item/ItemProvider.cs
@@ -20,7 +20,6 @@ public static class ItemProvider
 
         int randomNumber = (int)(1f + Random.value * (totalWeight - 1) + 0.5f);
 
-        Debug.Log($"선정된 랜덤 숫자: {randomNumber}");
         return randomNumber;
     }
 
@@ -44,21 +43,13 @@ public static class ItemProvider
         return item;
     }
 
-    public static void Test()
+    /// <summary>
+    /// 파라미터로 전달된 플레이어에게 랜덤한 아이템을 지급
+    /// </summary>
+    /// <param name="player">아이템을 지급받을 플레이어</param>
+    public static void GiveRandomItemTo(BoardgamePlayer player)
     {
-        int[] counts = new int[8];
-
-        for(int i = 0; i < 1000; ++i)
-        {
-            ItemData item = getRandomItem();
-            counts[item.Id] += 1;
-        }
-
-        int index = 0;
-
-        foreach(int count in counts)
-        {
-            Debug.Log($"{index++}: {count}");
-        }
+        ItemData item = getRandomItem();
+        player.Inventory.Add(item);
     }
 }
