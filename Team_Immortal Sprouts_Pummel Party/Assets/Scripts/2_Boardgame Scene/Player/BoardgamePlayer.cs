66a797445387f808d8c4eb7da08cc858f5a1746c
minyeong <minyk0512@gmail.com>
2023-06-20 15:54:42 +0900

    [Modify] PR 피드백 반영 (코드컨벤션, 조건문)
    1. 코드컨벤션 맞춰 수정
    2. 중첩된 if문 조건문 하나로 수합

--- Team_Immortal Sprouts_Pummel Party/Assets/Scripts/2_Boardgame Scene/Player/BoardgamePlayer.cs
+++ Team_Immortal Sprouts_Pummel Party/Assets/Scripts/2_Boardgame Scene/Player/BoardgamePlayer.cs
@@ -26,7 +26,7 @@ public class BoardgamePlayer : MonoBehaviour
 
     private void Start()
     {
-        UpdateCurrentIsland();
+        updateCurrentIsland();
         Inventory.InitInventory();
         isOnStartIsland = true;
     }
@@ -37,33 +37,30 @@ public class BoardgamePlayer : MonoBehaviour
     public void OnDiceStoped()
     {
         moveCount = dice.ConveyDiceReuslt();
-        HelpMoveAsync().Forget();
+        helpMoveAsync().Forget();
     }
 
     private Island currentIsland;
     private const int WAIT_TIME_BEFORE_MOVE = 1000;
     private bool canMoveOnDirectionIsland;
-    private async UniTaskVoid HelpMoveAsync()
+    private async UniTaskVoid helpMoveAsync()
     {
         await checkDepartureIsland();
 
-        CheckReachableIsland();
+        checkReachableIsland();
 
         await UniTask.Delay(WAIT_TIME_BEFORE_MOVE);
-        Move().Forget();
+        move().Forget();
     }
 
     private async UniTask<bool> checkDepartureIsland()
     {
-        if (currentIsland is RotationIsland)
+        if (currentIsland is RotationIsland && moveCount >= 1)
         {
-            if (moveCount >= 1)
-            {
-                currentIsland.GetComponent<RotationIsland>().PopUpDirectionArrow();
+            currentIsland.GetComponent<RotationIsland>().PopUpDirectionArrow();
 
-                await UniTask.WaitUntil(() => currentIsland.GetComponent<RotationIsland>().GetRotationStatus() == true);
-                canMoveOnDirectionIsland = true;
-            }
+            await UniTask.WaitUntil(() => currentIsland.GetComponent<RotationIsland>().GetRotationStatus() == true);
+            canMoveOnDirectionIsland = true;
         }
 
         return true;
@@ -80,7 +77,7 @@ public class BoardgamePlayer : MonoBehaviour
     }
 
     private const float MOVE_TIME = 0.5f;
-    private async UniTaskVoid Move()
+    private async UniTaskVoid move()
     {
         Vector3 start;
         Vector3 end;
@@ -88,7 +85,7 @@ public class BoardgamePlayer : MonoBehaviour
 
         if (currentIsland.GetCurrentPosition() == destIslandPosition)
         {
-            await LookForward();
+            await lookForward();
             return;
         }
 
@@ -99,15 +96,15 @@ public class BoardgamePlayer : MonoBehaviour
             float elapsedTime = 0f;
             start = rigidbody.position;
             end = destIslandPosition;
-            mid = GetMidPoint(start, end);
+            mid = getMidPoint(start, end);
 
             moveCount -= 1;
 
-            await LookNextDestIsland((end - start).normalized);
+            await lookNextDestIsland((end - start).normalized);
 
             while (elapsedTime <= MOVE_TIME)
             {
-                rigidbody.MovePosition(SecondaryBezierCurve(start, mid, end, elapsedTime / MOVE_TIME));
+                rigidbody.MovePosition(secondaryBezierCurve(start, mid, end, elapsedTime / MOVE_TIME));
                 elapsedTime += Time.deltaTime;
 
                 await UniTask.Yield();
@@ -115,18 +112,18 @@ public class BoardgamePlayer : MonoBehaviour
 
             await checkTransitIsland();
 
-            UpdateCurrentIsland();
-            CheckReachableIsland();
+            updateCurrentIsland();
+            checkReachableIsland();
             await UniTask.Yield();
         }
 
         animator.SetBool(BoardgamePlayerAnimID.IS_MOVING, false);
 
-        await LookForward();
+        await lookForward();
     }
 
     private const float ROTATE_TIME = 1f;
-    private async UniTask<bool> LookNextDestIsland(Vector3 dir)
+    private async UniTask<bool> lookNextDestIsland(Vector3 dir)
     {
         // 회전타일에서부터 출발하는 턴에서는 회전하지 않음
         if (currentIsland is RotationIsland && canMoveOnDirectionIsland)
@@ -154,10 +151,10 @@ public class BoardgamePlayer : MonoBehaviour
         return true;
     }
 
-    private Vector3 GetReverseVector(Vector3 vec) { return vec * -1f; }
-    private async UniTask<bool> LookForward()
+    private Vector3 getReverseVector(Vector3 vec) { return vec * -1f; }
+    private async UniTask<bool> lookForward()
     {
-        Vector3 lookDir = GetReverseVector(Vector3.forward);
+        Vector3 lookDir = getReverseVector(Vector3.forward);
         lookDir = lookDir.normalized;
 
         Quaternion start = transform.rotation;
@@ -178,27 +175,27 @@ public class BoardgamePlayer : MonoBehaviour
     }
 
     private const int JUMP_HEIGHT = 5;
-    private Vector3 GetMidPoint(Vector3 start, Vector3 end)
+    private Vector3 getMidPoint(Vector3 start, Vector3 end)
     {
         Vector3 mid = Vector3.Lerp(start, end, 0.5f);
         mid.y += JUMP_HEIGHT;
         return mid;
     }
 
-    private Vector3 PrimaryBezierCurve(Vector3 p1, Vector3 p2, float t)
+    private Vector3 primaryBezierCurve(Vector3 p1, Vector3 p2, float t)
     {
         return Vector3.Lerp(p1, p2, t);
     }
 
-    private Vector3 SecondaryBezierCurve(Vector3 p1, Vector3 p2, Vector3 p3, float t)
+    private Vector3 secondaryBezierCurve(Vector3 p1, Vector3 p2, Vector3 p3, float t)
     {
-        Vector3 m0 = PrimaryBezierCurve(p1, p2, t);
-        Vector3 m1 = PrimaryBezierCurve(p2, p3, t);
+        Vector3 m0 = primaryBezierCurve(p1, p2, t);
+        Vector3 m1 = primaryBezierCurve(p2, p3, t);
 
-        return PrimaryBezierCurve(m0, m1, t);
+        return primaryBezierCurve(m0, m1, t);
     }
 
-    private void UpdateCurrentIsland()
+    private void updateCurrentIsland()
     {
         RaycastHit hit;
         Physics.Raycast(transform.position, Vector3.down * 10f, out hit, int.MaxValue, LayerMask.GetMask("Island"));
@@ -216,7 +213,7 @@ public class BoardgamePlayer : MonoBehaviour
     private Vector3 destIslandPosition;
     // TODO: 죽었을 때 시작 섬으로 가니까 true로 바꿔줘야함
     private bool isOnStartIsland;
-    private void CheckReachableIsland()
+    private void checkReachableIsland()
     {
         if (moveCount >= 1)
         {
