b13531110ad7881bc0f51a6c0053b39ae4da3c98
rickythediver <wooseok1995@gmail.com>
2023-06-20 17:42:33 +0900

    [Modfiy] 코드중복 리팩토링(아직 더 해야함)

--- Team_Immortal Sprouts_Pummel Party/Assets/Scripts/2_Boardgame Scene/Emoji/EmojiController.cs
+++ Team_Immortal Sprouts_Pummel Party/Assets/Scripts/2_Boardgame Scene/Emoji/EmojiController.cs
@@ -1,20 +1,22 @@
 using Cysharp.Threading.Tasks;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class EmojiController : MonoBehaviour
 {
     [SerializeField] private RectTransform[] emojiButtonTransform;
-    [SerializeField] [Range(50f, 80f)] private float gapDistance = 70f;
-    [SerializeField] [Range(0.1f, 1f)] private float downTime = 0.3f;
+    [SerializeField][Range(50f, 80f)] private float gapDistance = 70f;
+    [SerializeField][Range(0.1f, 1f)] private float downTime = 0.3f;
 
     // 이모티콘은 1~4 까지
     private Vector3 initialPosition;
     private Vector3 lastPosition;
     private const int startIndex = 1;
     private const int lastIndex = 4;
-    private bool isEmojiDown = false;
+    [SerializeField] private bool isEmojiDown = false;
+    private bool isTouchable = true;
 
     private void Start()
     {
@@ -23,64 +25,66 @@ public class EmojiController : MonoBehaviour
 
     public void ListDownEmojiButtons()
     {
-        moveDown(initialPosition, startIndex, lastIndex, isEmojiDown).Forget();
+        if (isTouchable)
+        {
+            isTouchable = false;
+            if (!isEmojiDown)
+            {
+                moveDown(initialPosition, startIndex, lastIndex).Forget();
+            }
+            else
+            {
+                moveUp(lastPosition, startIndex, lastIndex).Forget();
+            }
+        }
     }
 
-    private async UniTaskVoid moveDown(Vector3 startPosition, int firstIndex, int lastIndex, bool isDown)
+    private async UniTaskVoid moveDown(Vector3 startPosition, int firstIndex, int lastIndex)
     {
-        if (!isDown)
+        if (lastIndex < firstIndex)
         {
-            if (lastIndex < firstIndex)
-            {
-                lastPosition = startPosition;
-                isEmojiDown = true;
-                return;
-            }
-
-            emojiButtonTransform[firstIndex].gameObject.SetActive(true);
-            
-            Vector3 targetPosition = startPosition - (Vector3.up * gapDistance);
-
-            float elapsedTime = 0f;
-
-            while (elapsedTime <= downTime)
-            {
-                emojiButtonTransform[firstIndex].position = Vector3.Lerp(startPosition, targetPosition, elapsedTime / downTime);
-                elapsedTime += Time.deltaTime;
-                await UniTask.Yield();
-            }
-
-            Vector3 newStartPosition = targetPosition - (Vector3.up * gapDistance);
-
-            moveDown(newStartPosition, firstIndex + 1, lastIndex, isDown).Forget();
+            isTouchable = true;
+            isEmojiDown = true;
+            lastPosition = emojiButtonTransform[firstIndex - 1].position;
+            return;
         }
-        else // 이미 내려와 있는 상태라면
-        {
-            if (lastIndex < firstIndex)
-            {
-                isEmojiDown = false;
-                return;
-            }
 
-            float elapsedTime = 0f;
+        emojiButtonTransform[firstIndex].gameObject.SetActive(true);
 
-            Vector3 newStartPosition = lastPosition;
-            Vector3 targetPosition = newStartPosition + (Vector3.up * gapDistance);
+        Vector3 targetPosition = startPosition - (Vector3.up * gapDistance);
 
-            while (elapsedTime <= downTime)
-            {
-                emojiButtonTransform[lastIndex].position = Vector3.Lerp(newStartPosition, targetPosition, elapsedTime / downTime);
-                elapsedTime += Time.deltaTime;
-                await UniTask.Yield();
-            }
+        await emojiMove(firstIndex, startPosition, targetPosition);
 
-            newStartPosition = targetPosition;
-
-            //moveDown(firstIndex, lastIndex - 1, isDown).Forget();
-
-        }
-        
-
+        moveDown(targetPosition, firstIndex + 1, lastIndex).Forget();
     }
 
+    private async UniTaskVoid moveUp(Vector3 startPosition, int firstIndex, int lastIndex)
+    {
+        if (lastIndex < firstIndex)
+        {
+            isTouchable = true;
+            isEmojiDown = false;
+            return;
+        }
+
+        Vector3 targetPosition = startPosition + (Vector3.up * gapDistance);
+
+        await emojiMove(lastIndex, startPosition, targetPosition);
+
+        emojiButtonTransform[lastIndex].gameObject.SetActive(false);
+
+        moveUp(targetPosition, firstIndex, lastIndex - 1).Forget();
+    }
+
+    private async UniTask emojiMove(int index, Vector3 startPosition, Vector3 targetPosition)
+    {
+        float elapsedTime = 0f;
+
+        while (elapsedTime <= downTime)
+        {
+            emojiButtonTransform[index].position = Vector3.Lerp(startPosition, targetPosition, elapsedTime / downTime);
+            elapsedTime += Time.deltaTime;
+            await UniTask.Yield();
+        }
+    }
 }
