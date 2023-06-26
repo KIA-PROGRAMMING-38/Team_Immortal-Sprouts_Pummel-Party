3c7ab6931c79d4bf38aa8d5b7898e6fbedf83e4e
minyeong <minyk0512@gmail.com>
2023-06-26 20:35:13 +0900

    [Feature] Die 상태 진입 시 셰이더 전환 및 기존 텍스쳐 적용

--- Team_Immortal Sprouts_Pummel Party/Assets/Scripts/2_Boardgame Scene/Player/DieBehaviour.cs
+++ Team_Immortal Sprouts_Pummel Party/Assets/Scripts/2_Boardgame Scene/Player/DieBehaviour.cs
@@ -2,5 +2,32 @@ using UnityEngine;
 
 public class DieBehaviour : StateMachineBehaviour
 {
-    
+    [SerializeField] private Material dissolveMaterial;
+    private Material originMaterial;
+
+    private GameObject playerBody;
+    private MeshRenderer[] bodyMeshes;
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        playerBody = animator.transform.GetChild(0).gameObject;
+        bodyMeshes = playerBody.GetComponentsInChildren<MeshRenderer>();
+
+        originMaterial = bodyMeshes[0].material;
+
+        foreach(MeshRenderer meshRenderer in bodyMeshes)
+        {
+            Texture originTexture = meshRenderer.material.mainTexture;
+            meshRenderer.material = dissolveMaterial;
+            meshRenderer.material.SetTexture("_Texture2D", originTexture);
+        }
+    }
+
+    public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+    }
+
+    public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        
+    }
 }
