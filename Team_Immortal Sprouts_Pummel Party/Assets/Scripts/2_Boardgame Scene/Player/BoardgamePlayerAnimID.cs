e1a1271cda6f977b597ad1642b3651b24287fc91
minyeong <minyk0512@gmail.com>
2023-06-26 21:46:31 +0900

    [Feature] 플레이어 Die 중간 커밋

--- Team_Immortal Sprouts_Pummel Party/Assets/Scripts/2_Boardgame Scene/Player/BoardgamePlayerAnimID.cs
+++ Team_Immortal Sprouts_Pummel Party/Assets/Scripts/2_Boardgame Scene/Player/BoardgamePlayerAnimID.cs
@@ -7,4 +7,5 @@ public static class BoardgamePlayerAnimID
     public static readonly int IS_MOVING = Animator.StringToHash("isMoving");
     public static readonly int DAMAGED = Animator.StringToHash("Damaged");
     public static readonly int DIE = Animator.StringToHash("Die");
+    public static readonly int DISSOLVED = Animator.StringToHash("Dissolved");
 }
--- Team_Immortal Sprouts_Pummel Party/Assets/Scripts/2_Boardgame Scene/Player/DieBehaviour.cs
+++ Team_Immortal Sprouts_Pummel Party/Assets/Scripts/2_Boardgame Scene/Player/DieBehaviour.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class DieBehaviour : StateMachineBehaviour
 {
     [SerializeField] private Material dissolveMaterial;
-    private Material originMaterial;
+    private Material[] originMaterials;
 
     private GameObject playerBody;
     private MeshRenderer[] bodyMeshes;
@@ -12,7 +12,11 @@ public class DieBehaviour : StateMachineBehaviour
         playerBody = animator.transform.GetChild(0).gameObject;
         bodyMeshes = playerBody.GetComponentsInChildren<MeshRenderer>();
 
-        originMaterial = bodyMeshes[0].material;
+        originMaterials = new Material[bodyMeshes.Length];
+        for(int i = 0; i < bodyMeshes.Length; ++i)
+        {
+            originMaterials[i] = bodyMeshes[i].material;
+        }
 
         foreach(MeshRenderer meshRenderer in bodyMeshes)
         {
@@ -22,12 +26,34 @@ public class DieBehaviour : StateMachineBehaviour
         }
     }
 
+    private const float DISSOLVE_DURATION = 2.5f;
+    private float elapsedTime;
+    private float dissolveRate;
     public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
+        dissolveRate = Mathf.Lerp(0f, 1f, elapsedTime / DISSOLVE_DURATION);
+        foreach(MeshRenderer meshRenderer in bodyMeshes)
+        {
+            meshRenderer.material.SetFloat("_DissolveAmount", dissolveRate);
+        }
+
+        elapsedTime += Time.deltaTime;
+
+        if(elapsedTime >= DISSOLVE_DURATION)
+        {
+            animator.SetTrigger(BoardgamePlayerAnimID.DISSOLVED);
+        }
     }
 
+    private int mateiralIndex = 0;
     public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        
+        elapsedTime = 0f;
+        animator.SetBool(BoardgamePlayerAnimID.DIE, false);
+
+        foreach (MeshRenderer meshRenderer in bodyMeshes)
+        {
+            meshRenderer.material = originMaterials[mateiralIndex++];
+        }
     }
 }
