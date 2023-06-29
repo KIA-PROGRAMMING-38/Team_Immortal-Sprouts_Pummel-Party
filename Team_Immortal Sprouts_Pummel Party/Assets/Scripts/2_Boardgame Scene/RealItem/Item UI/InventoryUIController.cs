55a056082f6371d77d30e1efc1fa17ef18c6ff64
rickythediver <wooseok1995@gmail.com>
2023-06-30 07:17:23 +0900

    [Add] 아이템 선택 UI 구현

--- Team_Immortal Sprouts_Pummel Party/Assets/Scripts/2_Boardgame Scene/RealItem/Item UI/InventoryUIController.cs
+++ Team_Immortal Sprouts_Pummel Party/Assets/Scripts/2_Boardgame Scene/RealItem/Item UI/InventoryUIController.cs
@@ -1,12 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.UI;
 
 public class InventoryUIController : MonoBehaviour
 {
     [SerializeField] private Canvas[] inventoryCanvas;
-    [SerializeField] private ItemSlot[] itemButtons;
 
     public BoardPlayerController player; // 지금은 임시로 한거임 => 보드게임매니저가 나중에 해줘야함
 
@@ -15,41 +15,40 @@ public class InventoryUIController : MonoBehaviour
     private const int CONTROL_CANVAS = 2;
 
     private int totalItemCount;
+    private int[] itemCountArray;
 
-    private void Awake()
-    {
-        itemButtons = GetComponentsInChildren<ItemSlot>();
-    }
+    public UnityEvent<int[]> OnEnableItemSlot = new UnityEvent<int[]>();
 
     private void Start()
     {
         totalItemCount = player.GetPlayerInventory().GetTotalItemCount();
+        itemCountArray = new int[totalItemCount];
     }
 
+    #region OnClick 이벤트 함수
+    /// <summary>
+    /// 아이템 버튼을 눌렀을 때 발동하는 함수
+    /// </summary>
     public void EnableItemSlotCanvas()
     {
         inventoryCanvas[ITEMSLOT_CANVAS].enabled = !inventoryCanvas[ITEMSLOT_CANVAS].enabled; // 껏다 켜주기
-        EnableItemSlot();
+        OnEnableItemSlot?.Invoke(getItemCounts());
     }
 
     public void TouchItemSlot()
     {
 
     }
+    #endregion
 
-
-    private void EnableItemSlot()
+    
+    private int[] getItemCounts()
     {
         for (int i = 0; i < totalItemCount; ++i) // 아이템 개수는 3개임
         {
-            if (1 <= player.GetPlayerInventory().GetItemCount(i)) // 아이템이 1개 이상이면
-            {
-                //itemButtons[i].interactable = true;
-            }
-            else // 아니면
-            {
-                //itemButtons[i].interactable = false;
-            }
+            itemCountArray[i] = player.GetPlayerInventory().GetItemCount(i);
         }
+
+        return itemCountArray;  
     }
 }
