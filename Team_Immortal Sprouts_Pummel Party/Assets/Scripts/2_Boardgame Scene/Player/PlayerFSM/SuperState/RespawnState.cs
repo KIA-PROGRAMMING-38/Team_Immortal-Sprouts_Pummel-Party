713336cee77be36e781fabd5872707c9b128d6e9
rickythediver <wooseok1995@gmail.com>
2023-06-30 01:55:26 +0900

    [Add] 리스폰 상태 수정

--- Team_Immortal Sprouts_Pummel Party/Assets/Scripts/2_Boardgame Scene/Player/PlayerFSM/SuperState/RespawnState.cs
+++ Team_Immortal Sprouts_Pummel Party/Assets/Scripts/2_Boardgame Scene/Player/PlayerFSM/SuperState/RespawnState.cs
@@ -1,17 +1,21 @@
 using Cysharp.Threading.Tasks;
 using System.Collections;
 using System.Collections.Generic;
+using UniRx.Triggers;
 using UnityEngine;
 
 public class RespawnState : PlayerState
 {
     public RespawnState(BoardPlayerController control, StateMachine machine, Animator anim, Rigidbody rigid, int animName) : base(control, machine, anim, rigid, animName)
     {
+        forwardDirection.Set(0f, 180f, 0f);
     }
 
+    private Vector3 forwardDirection;
     public override void Enter()
     {
         base.Enter();
+        playerController.transform.rotation = Quaternion.Euler(forwardDirection); // 정면 바라보게함
         changeToHoveringState().Forget();
     }
 
