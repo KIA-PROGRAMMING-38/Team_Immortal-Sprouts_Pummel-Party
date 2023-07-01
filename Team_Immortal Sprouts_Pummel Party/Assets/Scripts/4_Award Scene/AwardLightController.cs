624ac978989c9f9e274d5d1c56edcf77fe57824e
rickythediver <wooseok1995@gmail.com>
2023-07-02 04:12:44 +0900

    [Add] spotLight 랜덤 움직임 구현

--- Team_Immortal Sprouts_Pummel Party/Assets/Scripts/4_Award Scene/AwardLightController.cs
+++ Team_Immortal Sprouts_Pummel Party/Assets/Scripts/4_Award Scene/AwardLightController.cs
@@ -1,18 +1,69 @@
+using Cysharp.Threading.Tasks;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
+using TreeEditor;
 using UnityEngine;
 
 public class AwardLightController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] [Range(50f, 150f)] private float moveSpeed = 80f;
+    [SerializeField] private float minXRotation = 45f;
+    [SerializeField] private float maxXRotation = 135f;
+
+
+    private const float MIN_Y_ROTATION = 0f;
+    private const float MAX_Y_ROTATION = 359f;
+
+    private CancellationTokenSource playSource;
+    private CancellationTokenSource stopSource;
+    private CancellationToken token;
+
+    private void Awake()
     {
-        
+        playSource = new CancellationTokenSource();
+        stopSource = new CancellationTokenSource(); 
+        stopSource.Cancel();
+        token = playSource.Token;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+        lightRandomMove().Forget();
     }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            stopRandomMovement();
+        }
+    }
+
+    private async UniTask lightRandomMove()
+    {
+        token= playSource.Token;
+        Quaternion targetRotation = getRandomRotation();
+
+        while (3f <= Quaternion.Angle(transform.rotation, targetRotation))
+        {
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, moveSpeed * Time.deltaTime);
+            await UniTask.Yield(token);
+        }
+
+        lightRandomMove().Forget();
+    }
+
+    private Quaternion getRandomRotation()
+    {
+        Quaternion targetRotation;
+        float xValue = Random.Range(minXRotation, maxXRotation);
+        float yValue = Random.Range(MIN_Y_ROTATION, MAX_Y_ROTATION);
+        targetRotation = Quaternion.Euler(xValue, yValue, 0f);
+
+        return targetRotation;
+    }
+
+    private void stopRandomMovement() => token = stopSource.Token;  
+    
 }
