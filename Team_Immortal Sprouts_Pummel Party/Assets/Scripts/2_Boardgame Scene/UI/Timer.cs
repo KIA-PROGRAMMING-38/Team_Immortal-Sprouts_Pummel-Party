d0950c1c7d1cc592da67c036696d5affabb3f25e
0105kcy <ggbbsing@gmail.com>
2023-07-03 08:04:02 +0900

    [Feature] 타이머 애니메이션 추가
    1. 타이머 애니메이션 추가
    2. 타이머가 종료되면 캔버스 비활성화하고 미니게임으로 로드 레벨

--- Team_Immortal Sprouts_Pummel Party/Assets/Scripts/2_Boardgame Scene/UI/Timer.cs
+++ Team_Immortal Sprouts_Pummel Party/Assets/Scripts/2_Boardgame Scene/UI/Timer.cs
@@ -1,18 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Timer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Image countImage2;
+    [SerializeField] private Image countImage1;
+    [SerializeField] private Canvas informationCanvas;
+   
+    /// <summary>
+    /// 타이머 2 작동
+    /// </summary>
+    public void Timer2()
     {
-        
+        countImage2.gameObject.SetActive(true);
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// 타이머 1 작동
+    /// </summary>
+    public void Timer1()
     {
-        
+        countImage2.gameObject.SetActive(false);
+        countImage1.gameObject.SetActive(true);
+    }
+
+    /// <summary>
+    /// 타이머 끝
+    /// </summary>
+    public void TimeOver()
+    {
+        countImage1.gameObject.SetActive(false);
+        informationCanvas.gameObject.SetActive(false);
     }
 }
