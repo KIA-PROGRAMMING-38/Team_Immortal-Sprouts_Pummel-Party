b902aa50f573e2d3a13d24602c2a80e591cacebe
rickythediver <wooseok1995@gmail.com>
2023-07-03 13:42:07 +0900

    [Modify] LightBombSet 삭제
    프레임 드랍이 발생하여 눈물을 머금고 삭제

--- Team_Immortal Sprouts_Pummel Party/Assets/Scripts/4_Award Scene/AwardLightController.cs
+++ Team_Immortal Sprouts_Pummel Party/Assets/Scripts/4_Award Scene/AwardLightController.cs
@@ -1,8 +1,8 @@
 using Cysharp.Threading.Tasks;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Threading;
-using TreeEditor;
 using UnityEngine;
 
 public class AwardLightController : MonoBehaviour
@@ -10,13 +10,13 @@ public class AwardLightController : MonoBehaviour
     [SerializeField] private AwardProvider awardProvider;
 
     [Header("---------------------------Controllable---------------------------")]
-    [SerializeField] [Range(1f, 5f)] private float intensityControlTime = 2.5f;
-    [SerializeField] [Range(50f, 150f)] private float moveSpeed = 80f;
-    [SerializeField] [Range(30f, 50f)] private float minXRotation = 45f;
-    [SerializeField] [Range(80f, 95f)] private float maxXRotation = 90f;
-    [SerializeField] [Range(0.3f, 1f)] private float winnerReachTime = 0.5f;
+    [SerializeField][Range(1f, 5f)] private float intensityControlTime = 2.5f;
+    [SerializeField][Range(50f, 150f)] private float moveSpeed = 80f;
+    [SerializeField][Range(30f, 50f)] private float minXRotation = 45f;
+    [SerializeField][Range(80f, 95f)] private float maxXRotation = 90f;
+    [SerializeField][Range(0.3f, 1f)] private float winnerReachTime = 0.5f;
 
-    
+
     private const float MIN_Y_ROTATION = -90f;
     private const float MAX_Y_ROTATION = 90f;
 
@@ -28,24 +28,17 @@ public class AwardLightController : MonoBehaviour
 
     #region 불빛 세기
     private Light spotLight;
-    private float initialIntensity;
+    [SerializeField] private float initialIntensity = 30f;
 
     private Material lightRangeMaterial;
-    private float initialLightRangeOpacity;
+    [SerializeField] private float initialLightRangeOpacity = 0.02f;
     private const string OPACITY_KEY = "_Opacity";
     #endregion
 
 
     private void Awake()
     {
-        playSource = new CancellationTokenSource();
-        stopSource = new CancellationTokenSource(); 
-        stopSource.Cancel();
-        token = playSource.Token;
-        spotLight = GetComponent<Light>();
-        initialIntensity = spotLight.intensity;
-        lightRangeMaterial = transform.GetChild(0).GetComponent<MeshRenderer>().material;
-        initialLightRangeOpacity = lightRangeMaterial.GetFloat(OPACITY_KEY);
+        init();
     }
 
     private async void OnEnable()
@@ -67,8 +60,24 @@ public class AwardLightController : MonoBehaviour
 
     public float GetReachTime() => winnerReachTime;
 
+    private void init()
+    {
+        playSource = new CancellationTokenSource();
+        stopSource = new CancellationTokenSource();
+        stopSource.Cancel();
+        token = playSource.Token;
+        spotLight = GetComponent<Light>();
+        lightRangeMaterial = transform.GetChild(0).GetComponent<MeshRenderer>().material;
+    }
+
+    [SerializeField] private float waitTime = 2f;
     private async UniTask gradualLightIntensityControl()
     {
+        spotLight.intensity = 0f;
+        lightRangeMaterial.SetFloat(OPACITY_KEY, 0f);
+
+        await UniTask.Delay(TimeSpan.FromSeconds(waitTime));
+
         float elapsedTime = 0f; // 나중에 ExtensionMethod에 추가해야할듯
         while (elapsedTime <= intensityControlTime)
         {
@@ -83,7 +92,7 @@ public class AwardLightController : MonoBehaviour
 
     private async UniTask lightRandomMove()
     {
-        token= playSource.Token;
+        token = playSource.Token;
 
         Quaternion targetRotation = getRandomRotation();
 
@@ -99,8 +108,8 @@ public class AwardLightController : MonoBehaviour
     private Quaternion getRandomRotation()
     {
         Quaternion targetRotation;
-        float xValue = Random.Range(minXRotation, maxXRotation);
-        float yValue = Random.Range(MIN_Y_ROTATION, MAX_Y_ROTATION);
+        float xValue = UnityEngine.Random.Range(minXRotation, maxXRotation);
+        float yValue = UnityEngine.Random.Range(MIN_Y_ROTATION, MAX_Y_ROTATION);
         targetRotation = Quaternion.Euler(xValue, yValue, 0f);
 
         return targetRotation;
@@ -123,5 +132,5 @@ public class AwardLightController : MonoBehaviour
     {
         return start + (end - start) * t;
     }
-    
+
 }
--- Team_Immortal Sprouts_Pummel Party/Assets/Scripts/4_Award Scene/AwardProvider.cs
+++ Team_Immortal Sprouts_Pummel Party/Assets/Scripts/4_Award Scene/AwardProvider.cs
@@ -9,68 +9,50 @@ public class AwardProvider : MonoBehaviour
 {
     [SerializeField] private Transform[] playerTransforms;
     [SerializeField] private AwardLightController[] lightControllers;
-    [SerializeField] private ParticleSystem[] lightBombParticles;
 
     [HideInInspector]
     public UnityEvent<Transform> OnGiveAward = new UnityEvent<Transform>();
+    [HideInInspector]
     public UnityEvent OnAwardGiven = new UnityEvent();
-    [Range(0, 3)] public int testNumber;
 
     private void Awake()
     {
-        lightBombDelayTime = lightControllers[0].GetReachTime() + 0.5f;
     }
 
     private void Start()
     {
-
+        testAwardLoop().Forget();
     }
-
-    private void Update()
+    private async UniTaskVoid testAwardLoop()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        await UniTask.Delay(15000);
+
+        for (int i = 0; i <= SUB_AWARD_COUNT ;++i)
         {
-            giveAwardToPlayer(testNumber).Forget();
+            int playerNum = UnityEngine.Random.Range(0,4);
+            await giveAwardToPlayer(playerNum);
+            await UniTask.Delay(5000);
         }
     }
 
 
     private const int SUB_AWARD_COUNT = 3;
     private int awardCount;
-    private async UniTaskVoid giveAwardToPlayer(int playerEnterOrder)
+    private async UniTask giveAwardToPlayer(int playerEnterOrder)
     {
         Transform winnerTransform = playerTransforms[playerEnterOrder];
         OnGiveAward?.Invoke(winnerTransform); // 수상자를 spotLight으로 비춰준다
 
         if (SUB_AWARD_COUNT <= awardCount) // 마지막 상 수여
         {
-            await playLightBombParticles(winnerTransform);
-            await playLightBombParticles(winnerTransform);
-            await playLightBombParticles(winnerTransform);
-            await playLightBombParticles(winnerTransform);
+
         }
         else
         {
             ++awardCount;
-            await playLightBombParticles(winnerTransform);
             await UniTask.Delay(3000); // 테스트 => 플레이어의 승리 연출이 끝나면 으로 조건이 나중에 바껴야함
             OnAwardGiven?.Invoke(); // spotLight이 다시 랜덤하게 움직인다
         }
     }
 
-    private float lightBombDelayTime;
-    [SerializeField] private float lightBombGapTime = 0.1f;
-    private async UniTask playLightBombParticles(Transform winnerTransform)
-    {
-        await UniTask.Delay(TimeSpan.FromSeconds(lightBombDelayTime)); // 조명이 비출때까지 기다린다
-
-        for (int playCount = 0; playCount < lightBombParticles.Length; ++playCount)
-        {
-            Vector3 offSet = UnityEngine.Random.insideUnitCircle * 2f;
-            lightBombParticles[playCount].gameObject.SetActive(true); // 지가 알아서 자동으로 꺼지기 떄문에 SetActive(False) 불필요
-            lightBombParticles[playCount].transform.position = winnerTransform.position + offSet;
-            lightBombParticles[playCount].Play();
-            await UniTask.Delay(TimeSpan.FromSeconds(lightBombGapTime));
-        }
-    }
 }
