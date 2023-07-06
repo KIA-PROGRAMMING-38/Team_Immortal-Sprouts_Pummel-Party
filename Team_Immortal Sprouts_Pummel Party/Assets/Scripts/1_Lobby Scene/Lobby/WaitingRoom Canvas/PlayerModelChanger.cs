a809f2593389195d8b735765fd7c5f212a5fc0e3
rickythediver <wooseok1995@gmail.com>
2023-07-06 17:35:02 +0900

    [Modify] 기존 포톤 프리팹풀 삭제

--- Team_Immortal Sprouts_Pummel Party/Assets/Scripts/1_Lobby Scene/Lobby/WaitingRoom Canvas/PlayerModelChanger.cs
+++ Team_Immortal Sprouts_Pummel Party/Assets/Scripts/1_Lobby Scene/Lobby/WaitingRoom Canvas/PlayerModelChanger.cs
@@ -9,11 +9,11 @@ public class PlayerModelChanger : MonoBehaviourPunCallbacks
     [SerializeField] private Transform hatTransform;
     [SerializeField] private Material bodyMaterial;
     [SerializeField] private CustomData customData;
-    public DefaultPool defaultPrefabPool { get; private set; } // 포톤에서 제공하는 오브젝트 풀
+    //public DefaultPool defaultPrefabPool { get; private set; } // 포톤에서 제공하는 오브젝트 풀
 
     private void Awake()
     {
-        defaultPrefabPool = PhotonNetwork.PrefabPool as DefaultPool;
+        //defaultPrefabPool = PhotonNetwork.PrefabPool as DefaultPool;
     }
 
 
