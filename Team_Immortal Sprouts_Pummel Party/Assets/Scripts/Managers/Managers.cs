3157b6ab2fa66e83f37a55ed9c01cff684828080
rickythediver <wooseok1995@gmail.com>
2023-07-07 17:43:37 +0900

    [Feat] ResourceManager 구현

---
+++ Team_Immortal Sprouts_Pummel Party/Assets/Scripts/Managers/Managers.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Managers : MonoBehaviour
+{
+    private static Managers s_instance;
+
+    private static ResourceManager _resourceManager = new ResourceManager();
+    public static ResourceManager Resource { get { Init(); return _resourceManager; } }
+    
+    private void Awake()
+    {
+        Init();    
+    }
+
+    public static void Init()
+    {
+        if (s_instance is null)
+        {
+            GameObject go = GameObject.Find("@Managers");
+            if (go is null)
+            {
+                go = new GameObject("@Managers");
+                DontDestroyOnLoad(go);
+
+                s_instance = go.AddComponent<Managers>();
+
+                _resourceManager.Init();
+            }
+        }
+        
+    }
+}
---
+++ Team_Immortal Sprouts_Pummel Party/Assets/Scripts/Managers/ResourceManager.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class ResourceManager
+{
+    public void Init()
+    {
+
+    }
+
+    // TODO
+    // - 추후 자주 로드되는 리소스는 캐싱이 필요함.
+    // - 또한, Resources API가 아닌 AssetBundle을 사용해야 할 경우 로직이 변경될 수 있음.
+    public T Load<T>(string filename) where T : UnityEngine.Object
+    {
+        return Resources.Load<T>(filename);
+    }
+
+    public T Instantiate<T>(string filename) where T : UnityEngine.Object
+    {
+        var resource = Load<T>(filename);
+
+        return UnityEngine.Object.Instantiate(resource);
+    }
+    
+    public void Destroy(Object obj)
+    {
+        UnityEngine.Object.Destroy(obj);
+    }
+}
