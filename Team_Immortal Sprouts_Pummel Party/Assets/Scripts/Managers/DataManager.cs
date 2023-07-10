b97afd0d2c0d2fa87ce474e67f47a7865f1f6c91
rickythediver <wooseok1995@gmail.com>
2023-07-10 15:27:35 +0900

    [Add] CSVHelper 플러그인 설치

--- Team_Immortal Sprouts_Pummel Party/Assets/Scripts/Managers/DataManager.cs
+++ Team_Immortal Sprouts_Pummel Party/Assets/Scripts/Managers/DataManager.cs
@@ -1,6 +1,10 @@
+using CsvHelper;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Reflection;
 using UnityEngine;
@@ -21,6 +25,28 @@ public class DataManager
     {
         //Items = LoadCSV<ItemDataLoader, int, ItemData>("ItemTable").MakeDic();
         Bodies = LoadCSV<BodyDataLoader, int, BodyData>("BodyTable").MakeDic();
+    }
+
+    public List<T> ParseToList<T>([NotNull] string path)
+    {
+        using (var reader = new StreamReader(path))
+        {
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                return csv.GetRecords<T>().ToList();
+            }
+        }
+    }
+
+    public Dictionary<Key, Item> ParseToDict<Key, Item>([NotNull] string path, Func<Item, Key> keySelector)
+    {
+        using (var reader = new StreamReader(path))
+        {
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                return csv.GetRecords<Item>().ToDictionary(keySelector);
+            }
+        }
     }
 
     private Loader LoadCSV<Loader, Key, Item>(string name) where Loader : ILoader<Key, Item >, new()
