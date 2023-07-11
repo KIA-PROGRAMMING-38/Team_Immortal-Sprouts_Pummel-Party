5d49d11df620e45c8de19bb24868cf9661b4f09a
rickythediver <wooseok1995@gmail.com>
2023-07-11 17:55:42 +0900

    [Modify] 쓸모없는 using문 삭제

--- Team_Immortal Sprouts_Pummel Party/Assets/Scripts/Managers/DataManager.cs
+++ Team_Immortal Sprouts_Pummel Party/Assets/Scripts/Managers/DataManager.cs
@@ -5,7 +5,6 @@ using System.Diagnostics.CodeAnalysis;
 using System.Globalization;
 using System.IO;
 using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
 
 
 public interface ILoadble
