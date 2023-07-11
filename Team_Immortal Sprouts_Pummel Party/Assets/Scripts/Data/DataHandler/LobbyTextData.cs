cd1ddc98a1a620ffd4de5d31af500e549d1a45b6
rickythediver <wooseok1995@gmail.com>
2023-07-11 15:00:10 +0900

    [Add] LobbyTextData 추가

---
+++ Team_Immortal Sprouts_Pummel Party/Assets/Scripts/Data/DataHandler/LobbyTextData.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LobbyTextData
+{
+    public int ID { get; set; }
+    public string Text { get; set; }
+}
