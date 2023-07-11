0391b2961f1793674eecad01856ce2a3ce1c8323
rickythediver <wooseok1995@gmail.com>
2023-07-11 22:09:14 +0900

    [Add] UI 딕셔너리 추가

---
+++ Team_Immortal Sprouts_Pummel Party/Assets/Scripts/1_Lobby Scene/Lobby/NewLobby/LobbyUI.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LobbyUI : MonoBehaviour
+{
+    public Dictionary<string, Button> ButtonDict { get; set; }
+    public Dictionary<int, List<TMP_Text>> UITextDict { get; set; }
+
+    private void Start()
+    {
+        
+    }
+
+}
