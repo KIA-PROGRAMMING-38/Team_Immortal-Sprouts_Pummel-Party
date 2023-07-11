d5b27a135a5a0ccdd86d25bdd745a57ca9b9adc9
rickythediver <wooseok1995@gmail.com>
2023-07-11 12:47:38 +0900

    [Add] 매니저 생성 함수 추가

--- Team_Immortal Sprouts_Pummel Party/Assets/Scripts/Managers/Managers.cs
+++ Team_Immortal Sprouts_Pummel Party/Assets/Scripts/Managers/Managers.cs
@@ -13,23 +13,37 @@ public class Managers : MonoBehaviour
     private static DataManager _dataManager = new DataManager();
     public static DataManager Data { get { InitManagers(); return _dataManager; } }
 
+    private static PhotonManager _photonManager;
+    public static PhotonManager Photon { get { InitManagers(); return _photonManager; } }
 
     public static void InitManagers()
     {
         if (s_instance is null)
         {
             GameObject go = GameObject.Find("@Managers");
+
             if (go is null)
             {
                 go = new GameObject("@Managers");
                 DontDestroyOnLoad(go);
-
-                s_instance = go.AddComponent<Managers>();
-
-                _resourceManager.Init();
-                _dataManager.Init();
             }
-        }
 
+            s_instance = go.AddComponent<Managers>();
+
+            _photonManager = CreateComponentManager<PhotonManager>();
+
+            _resourceManager.Init();
+            _dataManager.Init();
+            _photonManager.Init();
+        }
+    }
+
+    public static T CreateComponentManager<T>() where T : UnityEngine.Component
+    {
+        GameObject managerGO = new GameObject($"{typeof(T)}");
+        managerGO.transform.SetParent(s_instance.transform);
+        T instance = managerGO.AddComponent<T>();
+
+        return instance;
     }
 }
